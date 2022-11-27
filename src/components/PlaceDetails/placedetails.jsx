faec8530b3b01754c36df06fdd364ccb141fe4a0
divyabeech <dyadav1006@gmail.com>
2022-11-27 09:49:24 -0700

    image

--- src/components/PlaceDetails/placedetails.jsx
+++ src/components/PlaceDetails/placedetails.jsx
@@ -1,8 +1,26 @@
 import React from 'react';
+import { Box, Typography, Button, Card, CardMedia, CardContent, CardActions, Chip } from '@material-ui/core';
+import LocationOnIcon from '@material-ui/icons/LocationOn';
+import PhoneIcon from '@material-ui/icons/Phone';
+import Rating from '@material-ui/lab/Rating';
+import useStyles from './styles.js';
+
 
 const Placedetails =({place}) => {
+    const classes = useStyles();
     return(
-        <h1>{place.name}</h1>
+        <Card elevation={6}>
+            <CardMedia
+                style={{height:350}}
+                image={place.photo ? place.photo.images.large.url : 'https://www.foodserviceandhospitality.com/wp-content/uploads/2016/09/Restaurant-Placeholder-001.jpg'}
+                
+            
+            
+            
+            />
+
+
+        </Card>
     )
 }
 
